0558d8f496856eb7d271b3ab624bef0261e75f51
xuqiushi <1527829777@qq.com>
2024-12-05 14:31:45 +0800

    🎨revise(删除一些没用的格式): 删除一些没用的格式
    删除一些没用的格式

--- types/components.d.ts
+++ types/components.d.ts
@@ -7,11 +7,8 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    ElAside: typeof import('element-plus/es')['ElAside']
     ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider']
     ElContainer: typeof import('element-plus/es')['ElContainer']
-    ElForm: typeof import('element-plus/es')['ElForm']
-    ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElInput: typeof import('element-plus/es')['ElInput']
     ElMain: typeof import('element-plus/es')['ElMain']
     RouterLink: typeof import('vue-router')['RouterLink']
